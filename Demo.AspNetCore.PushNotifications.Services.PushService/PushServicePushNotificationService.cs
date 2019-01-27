667852247c5160fce6170e365976ff391ccd25f5
Tomasz Pęczek <tpeczek@gmail.com>
2019-01-27 08:29:42 +0100

    Handle 410 and 404 responses from Push Service. Resolves #9

--- Demo.AspNetCore.PushNotifications.Services.PushService/PushServicePushNotificationService.cs
+++ Demo.AspNetCore.PushNotifications.Services.PushService/PushServicePushNotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -13,12 +14,13 @@ namespace Demo.AspNetCore.PushNotifications.Services.PushService
     {
         private readonly PushNotificationServiceOptions _options;
         private readonly PushServiceClient _pushClient;
+        private readonly IPushSubscriptionStoreAccessorProvider _subscriptionStoreAccessorProvider;
 
         private readonly ILogger _logger;
 
         public string PublicKey { get { return _options.PublicKey; } }
 
-        public PushServicePushNotificationService(IOptions<PushNotificationServiceOptions> optionsAccessor, PushServiceClient pushClient, IVapidTokenCache vapidTokenCache, ILogger<PushServicePushNotificationService> logger)
+        public PushServicePushNotificationService(IOptions<PushNotificationServiceOptions> optionsAccessor, PushServiceClient pushClient, IVapidTokenCache vapidTokenCache, IPushSubscriptionStoreAccessorProvider subscriptionStoreAccessorProvider, ILogger<PushServicePushNotificationService> logger)
         {
             _options = optionsAccessor.Value;
 
@@ -29,6 +31,7 @@ namespace Demo.AspNetCore.PushNotifications.Services.PushService
                 TokenCache = vapidTokenCache
             };
 
+            _subscriptionStoreAccessorProvider = subscriptionStoreAccessorProvider;
             _logger = logger;
         }
 
@@ -45,7 +48,28 @@ namespace Demo.AspNetCore.PushNotifications.Services.PushService
             }
             catch (Exception ex)
             {
-                _logger?.LogError(ex, "Failed requesting push message delivery to {0}.", subscription.Endpoint);
+                await HandlePushMessageDeliveryExceptionAsync(ex, subscription);
+            }
+        }
+
+        private async Task HandlePushMessageDeliveryExceptionAsync(Exception exception, PushSubscription subscription)
+        {
+            PushServiceClientException pushServiceClientException = exception as PushServiceClientException;
+
+            if (pushServiceClientException is null)
+            {
+                _logger?.LogError(exception, "Failed requesting push message delivery to {0}.", subscription.Endpoint);
+            }
+            else
+            {
+                if ((pushServiceClientException.StatusCode == HttpStatusCode.NotFound) || (pushServiceClientException.StatusCode == HttpStatusCode.Gone))
+                {
+                    using (IPushSubscriptionStoreAccessor subscriptionStoreAccessor = _subscriptionStoreAccessorProvider.GetPushSubscriptionStoreAccessor())
+                    {
+                        await subscriptionStoreAccessor.PushSubscriptionStore.DiscardSubscriptionAsync(subscription.Endpoint);
+                    }
+                    _logger?.LogInformation("Subscription has expired or is no longer valid and has been removed.");
+                }
             }
         }
     }
